0b0b873d54466a3447587dcc71e65cb6c9e944c6
zi-git100b <admin@zi-find.com>
2026-02-17 12:20:09 -0500

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import PawPilotApp from './App.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <PawPilotApp />
+  </React.StrictMode>,
+)
